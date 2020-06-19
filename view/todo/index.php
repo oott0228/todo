07a45587c564e19e5115e8fd0d36c2506f450f2d
oott0228 <oott0228@gmail.com>
2020-06-19 17:27:29 +0900

    完了ボタンのテスト

--- view/todo/index.php
+++ view/todo/index.php
@@ -34,6 +34,7 @@ $todo_list = $controller->index();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>TODOリスト</title>
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 </head>
@@ -53,7 +54,24 @@ $todo_list = $controller->index();
                     <?php echo $todo['title']; ?>
                 </a>:<?php echo $todo['display_status']; ?>
                 :締め切り:<?php echo $todo['deadline_date']; ?>
-                <button class="complete_btn" data-id="<?php echo $todo['id']; ?>">完了</button>
+
+                <input type="button" id="show" value="完了">
+                <div id="layer"></div>
+                <div id="popup">
+                    <div>完了しますか？</div>
+                    <input type="button" class="complete_btn" data-id="<?php echo $todo['id']; ?>">はい </input>
+                    <input type="button" class="close_btn" id="close">いいえ </input>
+                </div>
+
+                <!-- <div class="complete-popup" title="complete">
+                    <p>完了しますか?</p></div>
+                <button class="complete_btn" data-id="<?php echo $todo['id']; ?>">完了 </button>
+                <div class="complete-popup" title="complete">
+                <p>完了しますか?</p>
+                <div><button></button></div></div>
+                <button id="complete_btn" data-id="<?php echo $todo['id']; ?>">完了 </button>
+                 -->
+ 
                 <button class="delete_btn" data-id="<?php echo $todo['id']; ?>">削除</button>
             </li>  
             <?php endforeach;?>
@@ -65,15 +83,28 @@ $todo_list = $controller->index();
 </body>
 </html>
 <script>
-$(".complete_btn").on('click', function() {
-    alert('complete: ' + $(this).data('id'));
-    const todo_id = $(this).data('id');
-    window.location.href = "./index.php?action=complete&id=" + todo_id;
-});
+$(function() {
 
-$(".delete_btn").on('click', function() {
-    alert('delete: '+ $(this).data('id'));
+    $("#show").click(function(e) {
+        $('#show, #layer').show();
+    });
+
+    $(".complete_btn").on('click', function() {
+        // alert('complete: ' + $(this).data('id'));
+        const todo_id = $(this).data('id');
+        window.location.href = "./index.php?action=complete&id=" + todo_id;
+    });
+
+    $('#close, #layer').click(function(e) {
+        $('#popup, #layer').hide();
+    });
+
+    $("#delete_btn").on('click', function() {
+    // alert('delete: '+ $(this).data('id'));
     const todo_id = $(this).data('id');
     window.location.href = "./index.php?action=delete&id=" + todo_id;
+    });
+
 });
+
 </script>
