f200dd5f785de1af091258572702de48be59295f
oott0228 <oott0228@gmail.com>
2021-03-12 22:40:50 +0900

    インデント修正

--- controller/UserController.php
+++ controller/UserController.php
@@ -7,41 +7,41 @@ class UserController {
 
     public function new() {
 
-    if($_SERVER["REQUEST_METHOD"] !== "POST") {
-        return;
-    }
+        if($_SERVER["REQUEST_METHOD"] !== "POST") {
+            return;
+        }
         
-    $data = array(
-        "name" => $_POST['name'],
-        "email" => $_POST['email'],
-        "password" => $_POST['password'],
-        
-    );
+        $data = array(
+            "name" => $_POST['name'],
+            "email" => $_POST['email'],
+            "password" => $_POST['password'],
+            
+        );
 
-    $validation = new UserValidation;
-    $validation->setData($data);
-        
-    if ($validation->check() === false) {
-        $error_msgs = $validation->getErrorMessages();
-        session_start();
-        $_SESSION['error_msgs'] = $error_msgs;
-        header( "Location: ./new.php");
-        return;
-    }
+        $validation = new UserValidation;
+        $validation->setData($data);
+            
+        if ($validation->check() === false) {
+            $error_msgs = $validation->getErrorMessages();
+            session_start();
+            $_SESSION['error_msgs'] = $error_msgs;
+            header( "Location: ./new.php");
+            return;
+        }
 
-    $user = new User;
-    //$user->setUserId($user_id);
-    $user->setName($name);
-    $user->setPassword($password);
-    $user->setEmail($email);
-    $result = $user->save();
-    if ($result === false) {
-        $params = sprintf("?name=%s&password=%s&email=%s", $name, $password,$email);
-        header( "Location: ./new.php" . $params);
-    } else {
-        header( "Location: ../login/login.php" );
-    }
+        $user = new User;
+        //$user->setUserId($user_id);
+        $user->setName($name);
+        $user->setPassword($password);
+        $user->setEmail($email);
+        $result = $user->save();
+        if ($result === false) {
+            $params = sprintf("?name=%s&password=%s&email=%s", $name, $password,$email);
+            header( "Location: ./new.php" . $params);
+        } else {
+            header( "Location: ../login/login.php" );
+        }
     
-} 
+    } 
     
 }   
